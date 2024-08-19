a4deb39a95f02504f3715b6694a8daa85bb102d7
Nguyễn Văn Tùng <tungnv@eway.vn>
2024-08-20 00:26:13 +0700

    Add database config

--- src/main/java/com/template/demo/App.java
+++ src/main/java/com/template/demo/App.java
@@ -2,14 +2,8 @@ package com.template.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
-        DataSourceTransactionManagerAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
